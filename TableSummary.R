02b00a213e0aa767bda55bd9716c92baf1ee2dbe
Daniel Trejo <daniel.trejobanos@unil.ch>
2018-07-23 12:21:16 +0200

    adding credible intervals to tables

--- TableSummary.R
+++ TableSummary.R
@@ -1,5 +1,29 @@
+HDIofMCMC = function( sampleVec , credMass=0.95 ) {
+  # Computes highest density interval from a sample of representative values,
+  #   estimated as shortest credible interval.
+  # Arguments:
+  #   sampleVec
+  #     is a vector of representative values from a probability distribution.
+  #   credMass
+  #     is a scalar between 0 and 1, indicating the mass within the credible
+  #     interval that is to be estimated.
+  # Value:
+  #   HDIlim is a vector containing the limits of the HDI
+  sortedPts = sort( sampleVec )
+  ciIdxInc = ceiling( credMass * length( sortedPts ) )
+  nCIs = length( sortedPts ) - ciIdxInc
+  ciWidth = rep( 0 , nCIs )
+  for ( i in 1:nCIs ) {
+    ciWidth[ i ] = sortedPts[ i + ciIdxInc ] - sortedPts[ i ]
+  }
+  HDImin = sortedPts[ which.min( ciWidth ) ]
+  HDImax = sortedPts[ which.min( ciWidth ) + ciIdxInc ]
+  HDIlim = c( HDImin , HDImax )
+  return( HDIlim )
+}
+
 table.summary<-function(result){
-  
+   
   table.summary<-rbind(
   "Markers in model"=unclass(summary(result$perSampleMarkers)),
   "cpg probes in model"=unclass(summary(result$perSampleProbes)),
