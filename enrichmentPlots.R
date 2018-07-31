b7ed4b6f8b2ea9152b8ffc23385b7ce2dd73e8b3
Daniel Trejo <daniel.trejobanos@unil.ch>
2018-07-31 16:06:54 +0200

    posterior summaries

--- enrichmentPlots.R
+++ enrichmentPlots.R
@@ -45,14 +45,20 @@ topGO<-df
 #topGO$group<-ifelse(df$prob>0.95 & df$Mean>2.0,"Enriched","Non-enriched")
 #with ROPE
 topGO$group<-ifelse(df$prob>0.95 & df$ROPE<0.05,"Enriched","Non-enriched")
-ggplot(topGO, aes(x=prob, y=Mean,colour=group)) + geom_point() +ylab("Mean enrichment") + xlab("Posterior inclusion probability") + ggtitle("Enrichment of terms according to PIP for BMI")
-ggsave(filename="Enrichment_bmi.pdf")
+ribbon.df<-data.frame(ymin=rep(0.5,100),ymax=rep(1.5,100),x=seq(0,1,length.out = 100))
+p<-ggplot()+  geom_point(data=topGO, aes(x=prob, y=Mean,colour=group)) +ylab("Enrichment") + xlab("Posterior inclusion probability") + ggtitle("Enrichment of terms according to PIP for BMI")
+topGO$prob=as.numeric(topGO$prob)
+topGO[,"CI(l)"]<-as.numeric(topGO[,"CI(l)"])
+topGO[,"CI(h)"]<-as.numeric(topGO[,"CI(h)"])
+p<-p+geom_errorbar(data=topGO,aes(x=prob,ymin=topGO[,"CI(l)"],ymax=topGO[,"CI(h)"],colour=group))
+p<-p+geom_ribbon(data=ribbon.df,aes(ymin=ymin, ymax=ymax, x=x, fill = "ROPE"),linetype="solid",alpha=1.0)+scale_fill_manual(values=c( "grey80"), name="")
+ggsave(plot=p,filename="Enrichment_bmi.pdf")
 #topGO<-df[df$prob>0.95 & df$Mean>2.0,]
 topGO<-df[df$prob>0.95 & df$ROPE<0.05,]
 #topGO.output<-topGO[,c("go_id","Mean")]
 #write.csv(topGO.output,"GObmi.csv",row.names = F,sep = " ",quote = F)
 write_delim(x = topGO,delim = " ",path="GObmi.csv")#list of go terms for REVIGO
-
+print(xtable(topGO),file="GObmi.tex")
 enrich.df.Pprobes<-aggregate(P.probes ~ go_id,data=enrich.df,function(x)c(unclass(summary(x)),HDIofMCMC(x)) )
 #tmp<-as.data.frame(enrich.df.Pprobes$P.probes)
 if(is.matrix(enrich.df.agg$enrichment)){
@@ -109,7 +115,14 @@ df<-merge(enrich.df.smry,enrich.df.prob)
 topGO<-df
 #topGO$group<-ifelse(df$prob>0.95 & df$Mean>2.0,"Enriched","Non-enriched")
 topGO$group<-ifelse(df$prob>0.95 & df$ROPE<0.05,"Enriched","Non-enriched")
-ggplot(topGO, aes(x=prob, y=Mean,colour=group)) + geom_point() +ylab("Mean enrichment") + xlab("Posterior inclusion probability") + ggtitle("Enrichment of terms according to PIP for smoking")
+ribbon.df<-data.frame(ymin=rep(0.5,100),ymax=rep(1.5,100),x=seq(0,1,length.out = 100))
+p<-ggplot()+  geom_point(data=topGO, aes(x=prob, y=Mean,colour=group)) +ylab("Enrichment") + xlab("Posterior inclusion probability") + ggtitle("Enrichment of terms according to PIP for BMI")
+topGO$prob=as.numeric(topGO$prob)
+topGO[,"CI(l)"]<-as.numeric(topGO[,"CI(l)"])
+topGO[,"CI(h)"]<-as.numeric(topGO[,"CI(h)"])
+p<-p+geom_errorbar(data=topGO,aes(x=prob,ymin=topGO[,"CI(l)"],ymax=topGO[,"CI(h)"],colour=group))
+p<-p+geom_ribbon(data=ribbon.df,aes(ymin=ymin, ymax=ymax, x=x, fill = "ROPE"),linetype="solid",alpha=1.0)+scale_fill_manual(values=c( "grey80"), name="")
+
 ggsave(filename="Enrichment_smk.pdf")
 
 #topGO<-df[df$prob>0.95 & df$Mean>2.0,]
@@ -118,7 +131,7 @@ topGO<-df[df$prob>0.95 & df$ROPE<0.05,]
 #topGO.output<-topGO[,c("go_id","Mean")]
 #write.csv(topGO.output,"GObmi.csv",row.names = F,sep = " ",quote = F)
 write_delim(x = topGO,delim = " ",path="GOsmk.csv") #list of go terms for REVIGO
-
+print(xtable(topGO),file="GOsmk.tex")
 
 
 enrich.df.Pprobes<-aggregate(P.probes ~ go_id,data=enrich.df,function(x)c(unclass(summary(x)),HDIofMCMC(x)))
@@ -202,12 +215,13 @@ plot_tissue_enrichment<-function(input,output,title){
   topGO$group<-ifelse(df$prob>0.95 & df$ROPE<0.05,"Enriched","Non-enriched")
   ribbon.df<-data.frame(ymin=rep(0.5,100),ymax=rep(1.5,100),x=seq(0,1,length.out = 100))
   p<-ggplot()
-  p<-p+ geom_point(data=topGO, aes(x=prob, y=Mean,colour=group)) +ylab("Mean enrichment") + xlab("Posterior inclusion probability") + ggtitle(title)+theme_light()
-  p<-p+geom_ribbon(data=ribbon.df,aes(ymin=ymin, ymax=ymax, x=x, fill = "ROPE"), alpha = 0.3)
+  p<-p+ geom_point(data=topGO, aes(x=prob, y=Mean,colour=group)) +ylab("Enrichment") + xlab("Posterior inclusion probability") + ggtitle(title)+theme_light()
+ 
   topGO$prob=as.numeric(topGO$prob)
   topGO[,"CI(l)"]<-as.numeric(topGO[,"CI(l)"])
   topGO[,"CI(h)"]<-as.numeric(topGO[,"CI(h)"])
   p<-p+geom_errorbar(data=topGO,aes(x=prob,ymin=topGO[,"CI(l)"],ymax=topGO[,"CI(h)"],colour=group))
+  p<-p+geom_ribbon(data=ribbon.df,aes(ymin=ymin, ymax=ymax, x=x, fill = "ROPE"), alpha = 0.3)+scale_fill_manual(values=c( "grey80"), name="")
   ggsave(plot = p,filename = output[1]) 
   enrich.df.smry.pxv<-merge(enrich.df.smry.pxv,df)
   #with rope
@@ -216,20 +230,21 @@ plot_tissue_enrichment<-function(input,output,title){
   library(ggplot2)
   q<-ggplot(enrich.df.smry.pxv, aes(x=mean_pprobes,y=mean_pve,colour=group)) +geom_point()+geom_line(data=enrich.df.smry.pxv,aes(x=mean_pprobes,y=mean_pprobes,color="Expected"))+ylab("Mean proportion of PVE per term") +xlab("Mean proportion of probes mapping to term")+theme_light() + ggtitle(title)
   ggsave(plot=q,filename = output[2])
-  write_delim(x = enrich.df.smry.pxv,delim = " ",path=output[3])#list of go terms for REVIGO
+  write_delim(x = enrich.df.smry.pxv,delim = " ",path=output[3])
+  print(xtable(enrich.df.smry.pxv),file=output[4])
   list(table=enrich.df.smry.pxv, plot.prob=p, plot.prop=q)
 }
 
 
 
-en_bmi_o<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentGTEover.RData",c("probxenrich_GTE_o_bmi.pdf","propxpve_GTE_o_bmi.pdf","GTE_o_bmi.csv"), "Enrichment of GTEX over expressed genes for BMI")
+en_bmi_o<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentGTEover.RData",c("probxenrich_GTE_o_bmi.pdf","propxpve_GTE_o_bmi.pdf","GTE_o_bmi.csv","GTE_o_bmi.tex"), "Enrichment of GTEX over expressed genes for BMI")
 
-en_bmi_u<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentGTEunder.RData",c("probxenrich_GTE_u_bmi.pdf","propxpve_GTE_u_bmi.pdf","GTE_u_bmi.csv"), "Enrichment of GTEX repressed genes for BMI")
+en_bmi_u<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentGTEunder.RData",c("probxenrich_GTE_u_bmi.pdf","propxpve_GTE_u_bmi.pdf","GTE_u_bmi.csv","GTE_u_bmi.tex"), "Enrichment of GTEX repressed genes for BMI")
 
 
-en_smk_o<-plot_tissue_enrichment("../SNP+p_smk/enrichmentGTEover.RData",c("probxenrich_GTE_o_smk.pdf","propxpve_GTE_o_smk.pdf","GTE_o_smk.csv"), "Enrichment of GTEX over expressed genes for smoking")
+en_smk_o<-plot_tissue_enrichment("../SNP+p_smk/enrichmentGTEover.RData",c("probxenrich_GTE_o_smk.pdf","propxpve_GTE_o_smk.pdf","GTE_o_smk.csv","GTE_o_smk.tex"), "Enrichment of GTEX over expressed genes for smoking")
 
-en_smk_u<-plot_tissue_enrichment("../SNP+p_smk/enrichmentGTEunder.RData",c("probxenrich_GTE_u_smk.pdf","propxpve_GTE_u_smk.pdf","GTE_u_bmi.csv"),"Enrichment of GTEX repressed genes for BMI")
+en_smk_u<-plot_tissue_enrichment("../SNP+p_smk/enrichmentGTEunder.RData",c("probxenrich_GTE_u_smk.pdf","propxpve_GTE_u_smk.pdf","GTE_u_bmi.csv","GTE_u_smk.tex"),"Enrichment of GTEX repressed genes for BMI")
 
 order.table<-function(enrichment.table){enrichment.table[order(enrichment.table$Mean,decreasing = T),]}
 
@@ -240,18 +255,18 @@ order.table(en_smk_o$table)
 
 
 
-en_bmi_o_depict<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentDEPICTover.RData",c("probxenrich_DEPICT_o_bmi.pdf","propxpve_DEPICT_o_bmi.pdf","DEPICT_o_bmi.csv"), "Enrichment of DEPICT over expressed genes for BMI")
+en_bmi_o_depict<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentDEPICTover.RData",c("probxenrich_DEPICT_o_bmi.pdf","propxpve_DEPICT_o_bmi.pdf","DEPICT_o_bmi.csv","DEPICT_o_bmi.tex"), "Enrichment of DEPICT over expressed genes for BMI")
 order.table(en_bmi_o_depict$table)
 
-en_bmi_u_depict<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentDEPICTunder.RData",c("probxenrich_DEPICT_u_bmi.pdf","propxpve_DEPICT_u_bmi.pdf","DEPICT_u_bmi.csv"), "Enrichment of DEPICT repressed genes for BMI")
+en_bmi_u_depict<-plot_tissue_enrichment("../SNP+p_bmi_c/enrichmentDEPICTunder.RData",c("probxenrich_DEPICT_u_bmi.pdf","propxpve_DEPICT_u_bmi.pdf","DEPICT_u_bmi.csv","DEPICT_u_bmi.tex"), "Enrichment of DEPICT repressed genes for BMI")
 order.table(en_bmi_u_depict$table)
 
 
-en_smk_o_depict<-plot_tissue_enrichment("../SNP+p_smk/enrichmentDEPICTover.RData",c("probxenrich_DEPICT_o_smk.pdf","propxpve_DEPICT_o_smk.pdf","DEPICT_o_smk.csv"), "Enrichment of DEPICT over expressed genes for smoking")
+en_smk_o_depict<-plot_tissue_enrichment("../SNP+p_smk/enrichmentDEPICTover.RData",c("probxenrich_DEPICT_o_smk.pdf","propxpve_DEPICT_o_smk.pdf","DEPICT_o_smk.csv","DEPICT_o_smk.tex"), "Enrichment of DEPICT over expressed genes for smoking")
 order.table(en_smk_o_depict$table)
 
 
-en_smk_u_depict<-plot_tissue_enrichment("../SNP+p_smk/enrichmentDEPICTunder.RData",c("probxenrich_DEPICT_u_smk.pdf","propxpve_DEPICT_u_smk.pdf","DEPICT_u_smk.csv"), "Enrichment of DEPICT repressed genes for smoking")
+en_smk_u_depict<-plot_tissue_enrichment("../SNP+p_smk/enrichmentDEPICTunder.RData",c("probxenrich_DEPICT_u_smk.pdf","propxpve_DEPICT_u_smk.pdf","DEPICT_u_smk.csv","DEPICT_u_smk.tex"), "Enrichment of DEPICT repressed genes for smoking")
 order.table(en_smk_u_depict$table)
 
 
