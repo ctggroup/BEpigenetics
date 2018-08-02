250912f4d77675c4c6ceae951ff4279cc0fafa1c
Daniel Trejo banos <dtrejoba@dbc-serv03.vital-it.ch>
2018-08-02 10:11:13 +0200

    simulations scripts

---
+++ simulations_run_plot.r
@@ -0,0 +1,77 @@
+args = commandArgs(trailingOnly=TRUE)
+simulation_root=args[1]
+simulations=grep("sim_data",list.dirs(path=simulation_root),value=T)
+print(simulations)
+library(parallel)
+mc<-makeCluster(min(length(simulations),20))
+
+sim_list=parLapply(mc,simulations,function(x){res<-try(load(paste(x,"/posteriorSummary.RData",sep="")))
+  if(inherits(res, "try-error")){return(list(mse=NA,
+	          corB=NA,
+        	  adjR=NA,
+                  sigmaG=NA,
+                  sigmaE=NA,
+                  mseGwas=NA,
+                  corGwas=NA,
+                  adjRGwas=NA,
+                  varEGwas=NA,
+                  sigmaEGwas=NA,
+                  mseBlup=NA,
+                  corBlup=NA,
+                  adjRBlup=NA,
+                  varEBlup=NA,
+                  sigmaEBlup=NA,
+                  mseLasso=NA,
+                  corLasso=NA,
+                  adjRLasso=NA,
+                  varELasso=NA,
+                  sigmaELasso=NA,
+                  corR=NA,
+                  covR=NA,
+                  varEM=NA,
+                  corGRB=NA,
+                  corGRBlup=NA,
+                  corGRGwas=NA,
+                  corGRlasso=NA,
+                   corGRTruth=NA,
+                   corPRBlup=NA,
+		   corPRB=NA,
+		   corPRGwas=NA,
+	           corPRlasso=NA
+				))}
+     list(mse=posteriorSummary$mse_mean_B,
+	  corB=posteriorSummary$cor_B,
+          adjR=(posteriorSummary$lm_summary)$adj.r.squared,
+          sigmaG=posteriorSummary$meanSigmaG,
+          sigmaE=posteriorSummary$meanSigmaE,
+          mseGwas=posteriorSummary$mseGwas,
+	  corGwas=posteriorSummary$corGwas,
+	  adjRGwas=posteriorSummary$r2gwas,
+          varEGwas=posteriorSummary$varEGwas,
+          sigmaEGwas=posteriorSummary$sigmaEGwas,
+          mseBlup=posteriorSummary$mseBlup,
+	  corBlup=posteriorSummary$varEBlup,	
+	  adjRBlup=posteriorSummary$r2Blup,
+	  varEBlup=posteriorSummary$varEBlup,
+	  sigmaEBlup=posteriorSummary$sigmaEBlup,
+          mseLasso=posteriorSummary$mselasso,
+	  corLasso=posteriorSummary$corlasso,
+	  adjRLasso=posteriorSummary$r2lasso,
+	  varELasso=posteriorSummary$varElasso,
+	  sigmaELasso=posteriorSummary$sigmaElasso,
+	  corR=posteriorSummary$corR,
+	  covR=posteriorSummary$covR,   
+	  varEM=posteriorSummary$varEM,
+          corGRB=posteriorSummary$corGRB,
+          corGRBlup=posteriorSummary$corGRBlup,
+     	  corGRGwas=posteriorSummary$corGRGwas,     
+          corGRlasso=posteriorSummary$corGRlasso,
+	  corGRTruth=posteriorSummary$corGRTruth,
+          corPRBlup=posteriorSummary$corPRBlup,
+                   corPRB=posteriorSummary$corPRB,
+                   corPRGwas=posteriorSummary$corPRGwas,
+                   corPRlasso=posteriorSummary$corPRlasso
+	)})
+sim_df<-do.call(rbind,sim_list)
+rownames(sim_df)<-simulations
+save(list="sim_df",file=paste(simulation_root,"/simulationPlot.RData",sep=""))
